0051520b69657b0b3bd0e2b3fd1c5f01d60e3e6b
Cpapa97 <cpapa97@gmail.com>
2021-10-27 18:18:28 -0500

    trying top level setup.py again

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     description="an experiment with build-time feature flags",
     author="Christos Papadopoulos",
     url="https://github.com/Cpapa97/python-config-test.git",
-    packages=find_packages(exclude=["content"]),
+    packages=find_packages(),
     # include_package_data=True,
     install_requires=required,
     extras_require={"DJ": ["datajoint==0.12.9"]},
