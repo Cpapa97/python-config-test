90cf811b12b42f51b49b4b665d49186608eb93ff
Cpapa97 <cpapa97@gmail.com>
2021-10-27 18:26:19 -0500

    Add init to top level module

---
+++ minnie/__init__.py
@@ -0,0 +1,9 @@
+"""
+Top level module for this minnie test.
+"""
+
+from . import content
+from . import minnie_config as config
+
+def register():
+    print("Does nothing so far, but could be used to register certain things like the config's externals and adapters.")
